d8e8471e02866d6e9d110af75d1f65db0da89491
Thiago Contelli <thiagocontellid@gmail.com>
2024-04-28 17:00:31 -0300

    Get the movies and show it in the cards list

--- src/screens/RoomScreen.tsx
+++ src/screens/RoomScreen.tsx
@@ -1,5 +1,6 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Button, ImageBackground, Share, StyleSheet, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Button, ImageBackground, StyleSheet, View } from 'react-native';
 import Swiper from "react-native-deck-swiper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RootStackParamList } from "../../App";
@@ -8,61 +9,50 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Room'>;
 
 type Movie = {
   id: number
-  title: string
   poster: string
 }
 
 export function RoomScreen(props: Props) {
   const { params: { roomId } } = props.route
 
-  const movies: Movie[] = [
-    {
-      id: 1,
-      title: 'Inception',
-      poster: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg'
-    },
-    {
-      id: 2,
-      title: 'The Dark Knight',
-      poster: 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg'
-    },
-    {
-      id: 3,
-      title: 'Interstellar',
-      poster: 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg'
-    },
-    {
-      id: 4,
-      title: 'The Shawshank Redemption',
-      poster: 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg'
-    }
-  ]
+  const [movies, setMovies] = useState<Movie[]>([])
 
   function inviteToRoom(roomId: string) {
-    
+
   }
 
   function onSwipedRight(cardIndex: number) {
 
   }
 
+  async function getMovies() {
+    const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/Movies`)
+    const movies = await response.json()
+    setMovies(movies)
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
-      <Swiper
-        cards={movies}
-        renderCard={(card) => {
-          return (
-            <View style={styles.card}>
-              <ImageBackground source={{ uri: card.poster }} resizeMode="cover" style={styles.poster} />
-            </View>
-          )
-        }}
-        onSwipedRight={onSwipedRight}
-        disableTopSwipe
-        disableBottomSwipe
-        backgroundColor="gray"
-        childrenOnTop
-        stackSize={movies.length} />
+      {movies.length > 0 && (
+        <Swiper
+          cards={movies}
+          renderCard={(card) => {
+            return (
+              <View style={styles.card}>
+                <ImageBackground source={{ uri: card.poster }} resizeMode="cover" style={styles.poster} />
+              </View>
+            )
+          }}
+          onSwipedRight={onSwipedRight}
+          disableTopSwipe
+          disableBottomSwipe
+          backgroundColor="gray"
+          stackSize={movies.length} />
+      )}
 
       <View style={{ alignItems: 'center', justifyContent: 'center', padding: 16 }}>
         <Button onPress={() => inviteToRoom(roomId)} title="Invite" />
@@ -87,4 +77,4 @@ const styles = StyleSheet.create({
   poster: {
     flex: 1
   }
-});
+})
